4116222e6afd950792b4f65b85991db030aceaaf
jdelossantos22@email.arizona.edu <jdelossantos22@email.arizona.edu>
2021-10-22 20:59:16 -0700

    completed lab - i think. debugging/test cases are next

--- lab6/app.js
+++ lab6/app.js
@@ -4,14 +4,34 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const bodyParser = require('body-parser');     // Parses JSON in body
+// for mongDB
+const Recording = require("./models/recording");
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var labRouter = require('./routes/lab');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+// This is to enable cross-origin access
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // Pass to next layer of middleware
+  next();
+});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +41,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/lab', labRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
